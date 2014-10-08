0c5ce37e3e3d381fe5c558751c28d4d69c5e27e4
Mike Land <mikepland@gmail.com>
2014-10-08 14:35:15 -0500

    adds gesture recoginizer

---
+++ farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  farkle
+//
+//  Created by MIKE LAND on 10/8/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ farkle/DieLabel.m
@@ -0,0 +1,25 @@
+//
+//  DieLabel.m
+//  farkle
+//
+//  Created by MIKE LAND on 10/8/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+- (IBAction)onTapped:(UIGestureRecognizer *)sender {
+    NSLog(@"go time");
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
